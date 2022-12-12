e3f7ec46c5e5906bb0de0c1e79b6a21004369dd5
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2022-12-12 11:02:36 +0530

    Create string_compresiion_a3b2c2d1s1.java

---
+++ string_compresiion_a3b2c2d1s1.java
@@ -0,0 +1,46 @@
+ public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.nextLine();
+
+int sum=1;
+
+String out="";
+
+for(int i=0;i<s.length()-1;i++)
+
+{
+
+if(s.charAt(i)==s.charAt(i+1))
+
+{
+
+sum++;
+
+}
+
+else
+
+{
+
+out=out+s.charAt(i) ;
+
+out=out+sum;
+
+sum=1;
+
+}
+
+}
+
+out=out+s.charAt(s.length()-1);
+
+out=out+sum;
+
+System.out.println(out);
+
+}
+
+}
+    
